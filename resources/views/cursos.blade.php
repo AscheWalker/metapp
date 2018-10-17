948a9621d8a2e8cb5b20c4b963c7098df6215657
AscheWalker <tomas@walker.cl>
2018-10-17 05:46:04 -0300

    typo fix

--- resources/views/cursos.blade.php
+++ resources/views/cursos.blade.php
@@ -10,7 +10,7 @@
 				<div class="card-body">
 					<div class="btn-group-vertical btn-group-lg d-flex" role="group">
 						<a type="button" class="btn btn-secondary" href={{route('VerCursos')}}>Ver cursos</a>
-						<a type="button" class="btn btn-secondary" href={{route('CrearCursos')}}">Crear cursos</a>
+						<a type="button" class="btn btn-secondary" href={{route('CrearCursos')}}>Crear cursos</a>
 						<button type="button" class="btn btn-secondary">Editar cursos</button>
 					</div>
 				</div>
