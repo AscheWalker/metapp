19e621843b2c85fcb756743a49205b2ad8cfbbe5
AscheWalker <tomas@walker.cl>
2018-12-06 19:33:44 -0300

    alexa ssession table

---
+++ database/migrations/2018_12_06_222337_alexa_sessions.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlexaSessions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+		Schema::create('alexa_sessions', function (Blueprint $table) {
+            $table->increments('id');
+			$table->string('alexa', 999);
+			$table->integer('actividad');
+			$table->string('paso');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+		Schema::dropIfExists('alexa_sessions');
+    }
+}
