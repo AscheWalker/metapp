39dad134482415dea144ed1f4d5ae79b268e15c3
AscheWalker <tomas@walker.cl>
2018-12-13 20:33:50 -0300

    Cambio color welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #4697ca;
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
