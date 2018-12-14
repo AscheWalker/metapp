9f96b23d8cc641cc5bc27a5691f80a1974d821d7
AscheWalker <tomas@walker.cl>
2018-12-14 17:32:29 -0300

    fix missing parenthesis

--- resources/views/pasosCrear.blade.php
+++ resources/views/pasosCrear.blade.php
@@ -8,7 +8,7 @@
 				<div class="card-header">Crear Pasos</div>
 				
 				<div class="card-body">
-					<form action="/pasos/nuevo" method="GET">
+					<form action="/pasos/nuevo" method="POST">
 						<div class="form-group">
 							<label for="inputName">Nombre del paso </label>
 							<input type="text" class="form-control" id="inputName" name="name">
@@ -19,6 +19,8 @@
 						</div>
 							<input type="hidden" class="form-control" id="selectActivity" name="actividad" value={{ $idActiv }}>
 							<input type="hidden" class="form-control" id="inputOrder" name="order" value=1>
+							{{ csrf_field() }}
+							{{ method_field('PUT') }}
 						<button type="submit" class="btn btn-primary">Submit</button>
 					</form>
 				</div>
