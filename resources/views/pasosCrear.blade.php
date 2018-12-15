18d8a345e4ca0820298b30c06e19a5782b18fb58
AscheWalker <tomas@walker.cl>
2018-12-14 22:04:12 -0300

    arreglando bug de volver

--- resources/views/pasosCrear.blade.php
+++ resources/views/pasosCrear.blade.php
@@ -11,18 +11,18 @@
 					<form action="/pasos/nuevo" method="POST">
 						<div class="form-group">
 							<label for="inputName">Nombre del paso </label>
-							<input type="text" class="form-control" id="inputName" name="name">
+							<input type="text" class="form-control" id="inputName" name="name" required>
 						</div>
 						<div class="form-group">
 							<label for="inputDesc">Descripcion del paso </label>
-							<input type="text" class="form-control" id="inputDesc" name="desc">
+							<input type="text" class="form-control" id="inputDesc" name="desc" required>
 						</div>
 							<input type="hidden" class="form-control" id="selectActivity" name="actividad" value={{ $idActiv }}>
 							{{ csrf_field() }}
 							{{ method_field('PUT') }}
 						<div class="form-group">
 							<label for="inputOrder"> Order </label>
-							<input type="number" min="1" step="1" class="form-control" id="inputOrder" name="order">
+							<input type="number" min="1" step="1" class="form-control" id="inputOrder" name="order" required>
 						</div>
 						<button type="submit" class="btn btn-primary">Submit</button>
 					</form>
