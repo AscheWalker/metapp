dbaa7dc79ed4cf0a7aef8479c9fad8e21cb752ce
AscheWalker <tomas@walker.cl>
2019-01-10 20:34:12 -0300

    playing with sessions

--- app/Http/Controllers/SubPasosController.php
+++ app/Http/Controllers/SubPasosController.php
@@ -43,7 +43,7 @@ class SubPasosController extends Controller
 		$subpasos = SubPasos::create($request->all());
 		$idPaso = $request->pasos;
 		$characters = Pasos::where('actividad', $idPaso)->get();
-		return view('subPasosVer', compact('characters', 'idPaso', $_SESSION['idActiv']));
+		return view('subPasosVer', compact('characters', 'idPaso', session(['idActiv')));
     }
 
     /**
@@ -64,7 +64,7 @@ class SubPasosController extends Controller
 		$idActiv = $request->activId;
 		$idPaso = $request->pasoId;
 		$characters = SubPasos::where('pasos', $idPaso)->get();
-		$_SESSION['idActiv']=$idActiv;
+		session(['idActiv' => $idActiv]);
 		return view('subPasosVer', compact('characters', 'idPaso', 'idActiv'));
     }
 
